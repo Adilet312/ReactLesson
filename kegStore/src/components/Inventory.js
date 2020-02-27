d930484135fa6e774957349454240affb1765b1c
Student <example@example.com>
2020-02-26 17:29:53 -0800

    Add KegStore with React.

---
+++ kegStore/src/components/Inventory.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Inventory extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="top">
+                <h1>Inventory</h1>
+            </div>
+        );
+    }
+}
+export default Inventory;
---
+++ kegStore/src/components/StoreChoose.js
@@ -0,0 +1,28 @@
+import React from "react";
+class ChooseStore extends React.Component
+{
+    myInput = React.createRef();
+    goToStore = event =>{
+        event.preventDefault();
+        const storeName = this.myInput.value.value;
+        this.props.history.push(`/store/${storeName}`);
+    };
+    render() 
+    {
+        return (
+        
+        <form className="store-selector" onSubmit={this.goToStore}>
+            <h2> Please Enter Store</h2>
+            <input 
+                type="text" 
+                ref={this.myInput} 
+                required 
+                placeholder="Store Name" 
+                />
+            <button type="submit">Visit Store</button>
+        </form>
+        
+    );
+    }
+}
+export default ChooseStore;
